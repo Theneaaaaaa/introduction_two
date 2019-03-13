c3dcb576a7093d28bba38650c9ece7c2c8cc9c41
Thenea <thenea@iMac4.local>
2019-03-13 13:33:00 -0400

    Finished.

---
+++ Lab - Loops.playground/Pages/5. Exercise - Control Transfer Statements.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+/*:
+ ## Exercise - Control Transfer Statements
+ 
+ Create a for-in loop that will loop through the characters of `alphabet`, and print every other letter by continuing to the next iteration if you are on a letter you do not wish to print. (Hint: You can use the modulo operator to only print even indexed characters).
+ */
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for letter in alphabet {
+    
+}
+/*:
+ Create a `[String: String]` dictionary where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, with one of them being your home state. Now loop through this dictionary again, printing out the keys and values in a sentence, but add an if statement that will check if the current iteration is your home state. If it is, print("I found my home!") and break out of the loop.
+ */
+var strings: [String: String] = ["State 1":"City 1", "State 2":"City 2", "State 3":"City 3", "Maryland":"Sandy Spring"]
+for (State, City) in strings {
+    if State == "Maryland" {
+        print("I found my home! \(City)!")
+    }
+}
+//: [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Finding Movements](@next)
