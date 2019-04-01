d847588cbf998491e51d8af9ae23c498550ace0f
Thenea <thenea@iMac4.local>
2019-04-01 09:00:21 -0400

    finished

---
+++ Lab_Calculator/Lab_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab_Calculator
+//
+//  Created by Thenea on 3/13/19.
+//  Copyright © 2019 Thenea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
